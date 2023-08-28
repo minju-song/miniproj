da73d31b164595aeaf93972b1d7632b9e9ba7a43
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-08-29 00:54:25 +0900

    Add files via upload

--- 개발자1.sql
+++ 개발자1.sql
@@ -1,37 +1,40 @@
-select * from member;
-commit;
-
-insert into member(member_id, member_password, member_name)
-values ('minju', 'minju', 'º€πŒ¡÷');
-
-commit;
-
 create table food (
  food_id number primary key,
  food_name varchar2(20) not null,
  food_price number not null,
  food_script varchar2(50));
  
- select * from food;
- select * from employee;
- 
- SELECT * FROM EMPLOYEE WHERE EMP_ID = 'minju' AND EMP_PW = 'minju';
- 
- SELECT * FROM EMPLOYEE WHERE EMP_ID = 'minju';
- 
- insert into employee 
- values ('minju', 'minju', 'º€πŒ¡÷');
- 
- SELECT * FROM FOOD WHERE FOOD_NAME = '∂Û∏È';
- 
- INSERT INTO FOOD VALUES ((select MAX(food_id)+1 from food), '≈¡»ƒ∑Á', 3000, 'µ˛±‚');
- 
- select MAX(food_id)+1 from food;
- 
- insert into food
- values (1, '∂Û∏È', 3500, 'æ»º∫≈¡∏È');
- 
- create table employee (
+  create table employee (
  emp_id varchar2(20) primary key,
  emp_pw varchar2(20) not null,
  emp_name varchar2(20) not null);
+ 
+  INSERT INTO FOOD VALUES ((select MAX(food_id)+1 from food), '≈¡»ƒ∑Á', 3000, 'µ˛±‚');
+ 
+  insert into food
+ values (1, '∂Û∏È', 3500, 'æ»º∫≈¡∏È');
+ 
+  insert into employee 
+ values ('minju', 'minju', 'º€πŒ¡÷');
+ 
+ select * from food;
+ commit;
+ 
+ DELETE FROM food WHERE food_name = 'øÕ«√';
+ 
+ update food set food_sell = 0 where food_id = 3;
+ 
+ ALTER TABLE food ADD(food_sell NUMBER); 
+ 
+ update food set food_price = 5000, food_script = 'Ω≈∂Û∏È' where food_name = '∂Û∏È';
+ 
+ select * from employee;
+ 
+ create table orders (
+ order_id number primary key,
+ order_table number not null,
+ order_people number not null,
+ order_price number,
+ FOREIGN KEY (order_food) REFERENCES FOOD (FOOD_ID),
+ FOREIGN KEY (order_emp) REFERENCES employee (emp_id)
+ );
